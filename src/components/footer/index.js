480168bbfafcdcf0ab2c4f79037bb21bf95cc4a1
Arch1198 <108786876+Arch1198@users.noreply.github.com>
2023-01-19 18:46:56 +0530

    changes in footer file

---
+++ src/components/footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer flex absolute-center">
+      Made for you
+    </div>
+  );
+};
+
+export default Footer;
